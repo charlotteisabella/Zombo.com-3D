f36cdd0fc6d518520f5967cdaf6a5225a36c06a4
Charlotte Hicks <charlotteimccarthy@gmail.com>
2016-06-18 15:33:25 +1000

    Add orbit controls.

--- js/main.js
+++ js/main.js
@@ -1,60 +1,62 @@
 var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-      var renderer = new THREE.WebGLRenderer({ alpha: true });
-      renderer.setSize( window.innerWidth, window.innerHeight );
-      document.body.appendChild( renderer.domElement );
+var renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-      var geometry = new THREE.SphereGeometry( 1, 27, 27 );
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-      var greenMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      var redMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-      var blueMaterial = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
-      var orangeMaterial = new THREE.MeshBasicMaterial( { color: 0xff9900 } );
-      var blackMaterial = new THREE.MeshBasicMaterial( { color: 0x0f000f } );
-      var yellowMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-      var purpleMaterial = new THREE.MeshBasicMaterial( { color: 0x9933CC } );
+var geometry = new THREE.SphereGeometry( 1, 27, 27 );
 
-      var greenSphere = new THREE.Mesh( geometry, greenMaterial );
-      var redSphere = new THREE.Mesh( geometry, redMaterial );
-      var blueSphere = new THREE.Mesh( geometry, blueMaterial );
-      var orangeSphere = new THREE.Mesh( geometry, orangeMaterial );
-      var blackSphere = new THREE.Mesh( geometry, blackMaterial );
-      var yellowSphere = new THREE.Mesh( geometry, yellowMaterial );
-      var purpleSphere = new THREE.Mesh( geometry, purpleMaterial );
+var greenMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var redMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+var blueMaterial = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+var orangeMaterial = new THREE.MeshBasicMaterial( { color: 0xff9900 } );
+var blackMaterial = new THREE.MeshBasicMaterial( { color: 0x0f000f } );
+var yellowMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+var purpleMaterial = new THREE.MeshBasicMaterial( { color: 0x9933CC } );
 
-      scene.add( greenSphere );
-      scene.add( redSphere );
-      scene.add( blueSphere );
-      scene.add( orangeSphere );
-      scene.add( blackSphere );
-      scene.add( yellowSphere );
-      scene.add( purpleSphere );
+var greenSphere = new THREE.Mesh( geometry, greenMaterial );
+var redSphere = new THREE.Mesh( geometry, redMaterial );
+var blueSphere = new THREE.Mesh( geometry, blueMaterial );
+var orangeSphere = new THREE.Mesh( geometry, orangeMaterial );
+var blackSphere = new THREE.Mesh( geometry, blackMaterial );
+var yellowSphere = new THREE.Mesh( geometry, yellowMaterial );
+var purpleSphere = new THREE.Mesh( geometry, purpleMaterial );
 
-      blackSphere.position.y = 0;
+scene.add( greenSphere );
+scene.add( redSphere );
+scene.add( blueSphere );
+scene.add( orangeSphere );
+scene.add( blackSphere );
+scene.add( yellowSphere );
+scene.add( purpleSphere );
 
-      redSphere.position.y = 1.9;
-      redSphere.position.x = 1.05;
+blackSphere.position.y = 0;
 
-      blueSphere.position.y = -1.9;
-      blueSphere.position.x = 1.05;
+redSphere.position.y = 1.9;
+redSphere.position.x = 1.05;
 
-      orangeSphere.position.x = 2.1;
+blueSphere.position.y = -1.9;
+blueSphere.position.x = 1.05;
 
-      greenSphere.position.x = -2.1;
+orangeSphere.position.x = 2.1;
 
-      yellowSphere.position.y = 1.9
-      yellowSphere.position.x = -1.05
+greenSphere.position.x = -2.1;
 
-      purpleSphere.position.y = -1.9
-      purpleSphere.position.x = -1.05
+yellowSphere.position.y = 1.9
+yellowSphere.position.x = -1.05
+
+purpleSphere.position.y = -1.9
+purpleSphere.position.x = -1.05
 
 
-      camera.position.z = 13;
+camera.position.z = 10;
 
-      var render = function () {
-        requestAnimationFrame( render );
-        renderer.render(scene, camera);
-      };
+var render = function () {
+  requestAnimationFrame( render );
+  renderer.render(scene, camera);
+};
 
-      render();
+render();
